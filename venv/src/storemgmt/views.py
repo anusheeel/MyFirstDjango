339526d32b4714eeaec7d9f68e0b85da96ab9312
EmajoR <chapagainanusheel@gmail.com>
2020-11-22 09:15:03 +0545

    third-day

--- venv/src/storemgmt/views.py
+++ venv/src/storemgmt/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    #title = 'Welcome: This is the home page'
+    #first = 'This is my first django project'
+    #context = {
+        #"title" : title,
+        #"h1" : first
+    #}
+    return render(request, "index.html")
